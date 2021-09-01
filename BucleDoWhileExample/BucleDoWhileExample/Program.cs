76a3438395b696c454746f9d4c1b49391d974e9e
Hoffman Erwin <73903002+Coloradoo21@users.noreply.github.com>
2021-09-01 18:29:11 -0300

    Initial commit
    Example of loop do while on c#.

---
+++ BucleDoWhileExample/BucleDoWhileExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BucleDoWhileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int z = 10;
+
+            do
+            {
+                Console.WriteLine("impresion" + z);
+
+                z++;
+
+            } while (z < 15);
+
+        }
+
+    }
+}
