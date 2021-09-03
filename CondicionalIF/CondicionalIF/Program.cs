ea73d5c168e3706ae7913e14a9d13623b55bedfc
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-03 11:00:47 -0300

    IF test

---
+++ CondicionalIF/CondicionalIF/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Dynamic;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduce tu edad");
+
+            int edad = Int32.Parse(Console.ReadLine());
+
+            if (edad < 18) Console.WriteLine("Eres un niño");
+            
+           else if (edad < 30) Console.WriteLine("Eres joven");
+
+           else if (edad < 60) Console.WriteLine("Eres maduro");
+
+            else Console.WriteLine("Deber cuidarte ya");
+
+
+
+
+
+
+
+
+
+
+
+
+            /*string carnet="no";
+            Console.WriteLine("Vamos a evaluar si puedes conducir un vehiculo");
+            Console.WriteLine("Introduce tu edad por favor");
+            int edad = Int32.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+
+                Console.WriteLine("Tienes carnet?");
+                carnet = Console.ReadLine();
+            }
+            if(edad >=18 && carnet == "si")
+                Console.WriteLine("puedes conducir");
+            else Console.WriteLine("no puedes conducir vehiculos");
+            
+
+
+            
+             bool haceFrio;
+            haceFrio = false;
+
+            Console.WriteLine(haceFrio);*/
+
+
+
+
+        }
+    }
+}
