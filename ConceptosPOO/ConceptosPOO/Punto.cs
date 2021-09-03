f4857f8b1206174d815e4ba46e420dcdb8b2ee24
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-03 10:57:55 -0300

    POO Concept

---
+++ ConceptosPOO/ConceptosPOO/Punto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceptosPOO
+{
+    class Punto
+    {
+
+        private int x, y;
+
+        public Punto (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+          //Console.WriteLine($"Coordenada x: {x}, Coordenada y: {y}");
+
+
+        }
+
+        public double DistanciaHasta(Punto otroPunto)
+        {
+
+            int xDif = this.x - otroPunto.x;
+
+            int yDif = this.y - otroPunto.y;
+
+            double distanciaPuntos= Math.Sqrt(Math.Pow(xDif,2)+Math.Pow(yDif,2));
+
+            return distanciaPuntos;
+
+        }
+        public Punto()
+        {
+
+            this.x = 0;
+            this.y = 0;
+
+           //onsole.WriteLine("Este es el construcutor por defecto");
+
+
+        }
+
+
+    }
+}
