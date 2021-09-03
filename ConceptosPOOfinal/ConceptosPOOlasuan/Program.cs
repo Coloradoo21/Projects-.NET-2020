bbe02532fe923b199bf9d7df2ff22164253fae18
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-03 10:59:49 -0300

    POO concept final

---
+++ ConceptosPOOfinal/ConceptosPOOlasuan/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/*using static System.Math;
+
+using static System.Console;*/
+
+namespace ConceptosPOOlasuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // realizarTarea();
+
+            /*double raiz = Sqrt(9);
+
+            double potencia = Pow(3, 4);
+
+            WriteLine(raiz);
+            WriteLine(potencia);*/
+
+            var miVariable = new { Nombre = "Erwin", Edad = 25 };
+            Console.WriteLine(miVariable.Nombre+" "+miVariable.Edad);
+
+            var miOtraVariable = new { Nombre = "Romi", Edad = 22 };
+            miVariable = miOtraVariable;
+        }
+
+        static void realizarTarea()
+        {
+
+            Punto origen = new Punto();
+
+            Punto destino = new Punto(128, 80);
+
+            Punto otroPunto = new Punto();
+
+            double distancia = origen.DistanciaHasta(destino);
+
+            Console.WriteLine($"La distancia entre los puntos es de: {distancia}");
+
+            Console.WriteLine($"Numero de objetos creados: {Punto.ContadorDeObjetos()}");
+        }
+
+
+
+    }
+}
---
+++ ConceptosPOOfinal/ConceptosPOOlasuan/Punto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceptosPOOlasuan
+{
+    class Punto
+    {
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            contadorDeObjetos++;
+            // Console.WriteLine($"Coordenada x: {x}, coordenada y: {y}");
+        }
+
+        public Punto()
+        {
+
+            this.y = 0;
+            this.x = 0;
+            contadorDeObjetos++;
+            //Console.WriteLine("este es el constructor por defecto");
+
+        }
+
+        public double DistanciaHasta(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+
+            int yDif = this.y - otroPunto.y;
+
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
+
+            return distanciaPuntos;
+        }
+
+
+        private int x, y;
+
+        private static int contadorDeObjetos = 0;
+
+        /*public static int ContadorDeObjetos()
+        {
+            return contadorDeObjetos;
+        }*/
+
+        public static int ContadorDeObjetos() => contadorDeObjetos;
+
+        public const int constantePrueba = 7;
+
+
+    }
+}
