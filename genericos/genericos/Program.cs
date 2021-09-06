f4df8b2f3367c1480db9c24e2a5284517a84f09c
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-06 16:21:16 -0300

    First commit
    Asignar salarios a empleados con genericos.

---
+++ genericos/genericos/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlmacenObjetos archivos = new AlmacenObjetos(4);
+
+            /* archivos.agregar("Juan");
+
+             archivos.agregar("Fede");
+
+             archivos.agregar("Erwin");
+
+             archivos.agregar("Marcia");*/
+
+            archivos.agregar(new Empleado(1500));
+
+            archivos.agregar(new Empleado(2500));
+
+            archivos.agregar(new Empleado(3500));
+
+            archivos.agregar(new Empleado(4500));
+
+            string nombrePersona =(string) archivos.getElemento(2);
+
+            Console.WriteLine(nombrePersona);
+        }
+    }
+
+    class AlmacenObjetos
+    {
+        public AlmacenObjetos(int z)
+        {
+            datosElemento = new object[z];
+        }
+
+        public void agregar(object obj)
+        {
+            datosElemento[i] = obj;
+            i++;
+        }
+        public object getElemento(int i)
+        {
+            return datosElemento[i];
+        }
+
+
+        private object[] datosElemento;
+
+        private int i = 0;
+    }
+
+    class Empleado
+    {
+        public double getSalario()
+        {
+            return salario;
+        }
+        
+        private double salario;
+
+        public Empleado(double salario)
+        {
+
+            this.salario = salario;
+
+        }
+    }
+    
+}
