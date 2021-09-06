884bcaba94185f5d41e8fa55edac3e71ef4e6378
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-06 16:18:25 -0300

    First commit
    Expresiones regulares.

---
+++ Expresiones Regulares/Expresiones Regulares/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Expresiones_Regulares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Mi web es: http://pildorasinformaticas.es";
+
+            string patron = "https://(www.)?pildorasinformaticas.es";
+
+            Regex miRegex = new Regex(patron);
+
+            MatchCollection elMatch = miRegex.Matches(frase);
+
+            if (elMatch.Count > 0) Console.WriteLine("Se ha encontrdo web");
+            
+                else Console.WriteLine("No se ha encontrado web");
+            
+        }
+    }
+}
