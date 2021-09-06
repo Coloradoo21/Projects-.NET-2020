0264da5a5770ca2958cb5e3e0ef02b9de200b6a3
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-06 13:14:59 -0300

    Destroyers test

---
+++ Destructores/Destructores/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Destructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos miarchivo = new ManejoArchivos();
+
+            miarchivo.mensaje();
+        }
+    }
+
+    class ManejoArchivos
+    {
+
+        StreamReader archivos = null;
+
+        int contador = 0;
+
+        string linea;
+
+        public ManejoArchivos()
+        {
+
+            archivos = new StreamReader(@"c:\texto.txt");
+
+            while ((linea = archivos.ReadLine()) != null)
+            {
+                Console.WriteLine(linea);
+
+                contador++;
+            }
+        }
+
+        public void mensaje()
+        {
+            Console.WriteLine("hay {0} lineas", contador);
+        }
+
+        ~ManejoArchivos()
+        {
+            archivos.Close();
+        }
+            
+
+    }
+
+
+}
