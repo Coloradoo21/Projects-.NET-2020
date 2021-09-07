ee52d9b31734a2e47b3fe6b2d6293b1f5fb6d28d
Hoffman Erwin <73903002+HOFFMAN-Erwin@users.noreply.github.com>
2021-09-07 19:58:46 -0300

    First commit
    Practica de propiedades.

---
+++ PracticaProperties/PracticaProperties/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace PracticaProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado Juan = new Empleado("Juan");
+            Juan.SALARIO = 1200;
+
+            Console.WriteLine("El salario del empleado es de:" + Juan.SALARIO);
+
+        }
+    }
+
+    class Empleado
+    {
+        private string Nombre;
+        private double Salario;
+        public Empleado(string nombre)
+        {
+
+            this.Nombre = nombre;
+
+        }
+
+       /* public double SALARIO
+        {
+            get{ return this.Salario; }
+            set { this.Salario = EvaluaSalario(value); }
+
+        }*/
+
+        public double SALARIO
+        {
+            get => this.Salario;
+
+            set => this.Salario = EvaluaSalario(value);
+
+
+        }
+
+        private double EvaluaSalario(double Salario)
+        {
+            if (Salario < 0) return 0;
+            else return Salario;
+        }
+
+        /*public void SetSalario(double Salario)
+        {
+            if (Salario < 0)
+            {
+                Console.WriteLine("El salario no puede ser negativo, se asigno salario 0.");
+                this.Salario = 0;
+            }
+            else 
+            {
+                this.Salario = Salario;
+            }
+
+
+        }
+        public double GetSalario()
+        {
+            return Salario;
+        }*/
+
+
+    }
+}
